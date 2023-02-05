273f2855ffca4a9349cb1cc0ab7fb0fbad5dab5a
beraoudabdelkhalek <a.beraoud@esi-sba.dz>
2023-02-05 12:53:22 +0100

    companie route done

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const dotenv=require('dotenv')
+//const dotenv=require('dotenv')
 const app=require('./app.js')
-dotenv.config({path: './config.env'})
+//dotenv.config({path: './config.env'})
 
-const PORT=process.env.PORT || 8000
+//const PORT=process.env.PORT || 8000
 app.listen(8000,()=>{
-    console.log(`listening in port ${PORT}`)
+    console.log(`listening in port 8000`)
 })

dbc55c7c7a85426f4482401765f74570e43ee7a5
Abdelkarim17B <la_bengherbia@esi.dz>
2023-02-09 00:55:45 +0100

    second

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,7 +4,9 @@ import Image from "next/image";
 import Auth from "@/pages/api/Auth";
 import Card from "@/components/Card";
 import Navbar from "@/components/layout/navbar";
+import Footer from "@/components/layout/footer";
+import Home from "@/pages/api/Home";
 
 export default function Home() {
-  return <Guest />;
+  return <Home />;
 }
